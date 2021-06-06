c4acd9ec3197c266972be3f4291c26288cb8a81c
Saravanan S <sec20cb018@sairamtap.edu.in>
2021-06-06 12:55:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This is bootcamp was taken by Shaurya Sinha. He was our instructor. He
+        taught us html,css,javascript and react.js. He was very talented in
+        teaching this. i like the way teaches.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
